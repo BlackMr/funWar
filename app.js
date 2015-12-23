da2a3283030fef5f48580199d39d44e1a4c632bf
Kaan Karabey <karabeykaan@gmail.com>
2015-12-23 15:34:55 +0200

    some modules added to server side

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var mongoose = require('mongoose');
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override');
 mongoose.connect('mongodb://localhost/myAppDatabase');
+//mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
 
+
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+    app.use(morgan('dev'));                                         // log every request to the console
+    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
+
+ 
 app.get('/', function (req,res)
-{	
-	res.sendFile(__dirname + '/index.html');
+{ 
+  res.sendFile(__dirname + '/index.html');
 
 });
-
-
 
 // if our user.js file is at app/models/user.js
 var User = require('./app/models/user');
@@ -20,9 +31,9 @@ var randomUsername = "randomuser" + String(Math.random()*100000);
  
 // create a new user
 var newUser = User({
-  name: 'ahmeat',
+  name: "ahmett",
   username: randomUsername.substring(0,15),
-  password: 'amcık andyasdasd',
+  password: '123456',
   admin: true
 });
 
@@ -31,8 +42,6 @@ var newUser = User({
 // user will now be Chris-dude
 newUser.dudify(function(err, name) {
   if (err) throw err;
-
-  console.log('Your new name is ' + name);
 });
 
 
@@ -41,6 +50,7 @@ newUser.save(function(err) {
   if (err) throw err;
   console.log(newUser);
   console.log('User created!');
+  console.log('Your new name is ' + newUser.name);
 });
 
 
